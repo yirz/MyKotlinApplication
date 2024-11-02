fe0892f55f4eee8feb02aa3140fb5bcc605cd315
yirz <yann.irz@gmail.com>
2024-11-02 18:29:48 +0100

    top bar fonctionelle sans recherche

--- app/src/main/java/irz/test/myapplication/MainActivity.kt
+++ app/src/main/java/irz/test/myapplication/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.BottomAppBar
@@ -28,112 +30,166 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val viewmodel: MainViewModel by viewModels()
 
         setContent {
-            MyApp()
-        }
-    }
-}
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    val navController: NavHostController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
+            val navController: NavHostController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+            var text by remember { mutableStateOf("") }
+            var active by remember { mutableStateOf(false) }
 
-    Scaffold (
-        bottomBar = {
-            if(currentDestination?.route != "Profil") {
-                BottomNavigation(contentColor = MaterialTheme.colorScheme.primary,)
-                //item profil
-                { BottomNavigationItem(
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Face,
-                            contentDescription = "icone profil",
-                            modifier = Modifier.size(35.dp),
-                            tint = Color.Black,
-                        )
-                    },
-                    label = { Text("Profil") },
-                    selected = false,
-                    onClick = {
-                        navController.navigate("Profil")
 
-                    })
-                    //item Films
-                    BottomNavigationItem(
-                        icon = {
-                            Image(
-                                painterResource(id = R.drawable.films),
-                                contentDescription = "logo Films",
-                                modifier = Modifier.size(35.dp) // Adjust the size here
+            Scaffold(
+                topBar = {
+                    if (currentDestination?.route != "Profil")
+                        SearchBar(
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "icone search"
+                                )
+                            },
+                            trailingIcon = {
+                                Icon(
+                                    modifier = Modifier.clickable {
+                                        if (text.isNotEmpty()) {
+                                            text = ""
+                                        } else {
+                                            active = false
+                                        }
+                                    },
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "icone fermeture"
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            query = text,
+                            onQueryChange = { text = it },
+                            onSearch = {
+                                if (currentDestination?.route == "Films") {
+                                    viewmodel.recherche_films(it)
+                                    active = false
+                                } else if (currentDestination?.route == "Acteurs") {
+                                    viewmodel.recherche_acteurs(it)
+                                    active = false
+                                } else if (currentDestination?.route == "Series") {
+                                    viewmodel.recherche_acteurs(it)
+                                    active = false
+                                }
+                            },
+                            active = active,
+                            onActiveChange = { active = it },
+                            placeholder = {
+                                Text(text = "Search")
+                            }
+                        ) {
+                        }
+                },
+                bottomBar = {
+                    if (currentDestination?.route != "Profil") {
+                        BottomNavigation(contentColor = MaterialTheme.colorScheme.primary) {
+                            // item profil
+                            BottomNavigationItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Face,
+                                        contentDescription = "icone profil",
+                                        modifier = Modifier.size(35.dp),
+                                        tint = Color.Black,
+                                    )
+                                },
+                                label = { Text("Profil") },
+                                selected = false,
+                                onClick = {
+                                    navController.navigate("Profil")
+                                }
                             )
-                        },
-                        label = { Text("Films") },
-                        selected = false,
-                        onClick = {
-                            navController.navigate("Films")
-                        })
-
-                    //item Series
-                    BottomNavigationItem(
-                        icon = {
-                            Image(
-                                painterResource(id = R.drawable.series),
-                                contentDescription = "logo series",
-                                modifier = Modifier.size(35.dp) // Adjust the size here
+                            // item Films
+                            BottomNavigationItem(
+                                icon = {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.films),
+                                        contentDescription = "logo Films",
+                                        modifier = Modifier.size(35.dp)
+                                    )
+                                },
+                                label = { Text("Films") },
+                                selected = false,
+                                onClick = {
+                                    navController.navigate("Films")
+                                }
                             )
-                        },
-                        label = { Text("Series") },
-                        selected = false,
-                        onClick = {
-                            navController.navigate("Series")
-                        })
-
-                    //item Actors
-                    BottomNavigationItem(
-                        icon = {
-                            Image(
-                                painterResource(id = R.drawable.actor),
-                                contentDescription = "logo acteurs",
-                                modifier = Modifier.size(35.dp) // Adjust the size here
+                            // item Series
+                            BottomNavigationItem(
+                                icon = {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.series),
+                                        contentDescription = "logo series",
+                                        modifier = Modifier.size(35.dp)
+                                    )
+                                },
+                                label = { Text("Series") },
+                                selected = false,
+                                onClick = {
+                                    navController.navigate("Series")
+                                }
                             )
-                        },
-                        label = { Text("Acteurs") },
-                        selected = currentDestination?.route == "Acteurs",
-                        onClick = {
-                            navController.navigate("Acteurs")
-                        })
+                            // item Actors
+                            BottomNavigationItem(
+                                icon = {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.actor),
+                                        contentDescription = "logo acteurs",
+                                        modifier = Modifier.size(35.dp)
+                                    )
+                                },
+                                label = { Text("Acteurs") },
+                                selected = currentDestination?.route == "Acteurs",
+                                onClick = {
+                                    navController.navigate("Acteurs")
+                                }
+                            )
+                        }
+                    }
                 }
-
+            ) { paddingValues ->
+                NavHost(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(paddingValues),
+                    navController = navController,
+                    startDestination = "Profil"
+                ) {
+                    composable("Profil") { Screen(navController) }
+                    composable("Films") { Films(navController, viewModel()) }
+                    composable("Series") { Series(navController, viewModel()) }
+                    composable("Acteurs") { Acteurs(navController, viewModel()) }
+                }
             }
         }
-            ){
-        NavHost(
-            modifier = modifier,
-            navController = navController,
-            startDestination = "Profil"
-        ) {
-            composable("Profil") { Screen(navController) }
-            composable("Films") { Films(navController, viewModel())}
-            composable("Series") {Series(navController, viewModel())}
-            composable("Acteurs") {Acteurs(navController, viewModel())}
 
         }
-
     }
 
-}
+
